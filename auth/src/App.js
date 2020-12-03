e5feffdf4814ecda4a5bd931a2f5886e7a117e75
GibJaf <gibraan.jafar@viit.ac.in>
2020-12-03 13:04:25 +0530

    Completed the auth app

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,17 @@
+/* 
+To Fix : 
+Enlarge the logout button so that it can be pressed.
+*/
+
 import firebase from 'firebase';
 import React, {Component} from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+    state = { loggedIn: null};
 
     componentDidMount(){
         firebase.initializeApp({
@@ -17,13 +24,43 @@ class App extends Component {
             appId: "1:302666603582:web:82d21dbc03eebb79a6056f",
             measurementId: "G-9CMCRRL6SM"
           });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user){
+                this.setState({ loggedIn: true});
+            } else {
+                this.setState({ loggedIn: false});
+            }
+        });
     }
+
+    renderContent() {
+        switch(this.state.loggedIn){
+            case true: 
+                return (
+                <View>
+                    <Button onPress={() => {
+                    firebase.auth().signOut();
+                    console.log("Log Out pressed");
+                }}
+                >Log Out</Button>
+                
+                </View>
+                );
+            case false: 
+                return <LoginForm />
+            default:
+                return <Spinner size="large"/>;
+
+        }
+    }
+
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm/>
+                {this.renderContent()}
             </View>
         );
     }
