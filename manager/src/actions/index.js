c0f0a57c78e1460053740023ee7386e4168da0c5
GibJaf <gibraan.jafar@viit.ac.in>
2020-12-28 11:31:46 +0530

    Finished till Folder 31 video 8

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-
+import { Actions } from 'react-native-router-flux';
 import { 
     EMAIL_CHANGED,
     PASSWORD_CHANGED,
@@ -26,7 +26,7 @@ export const loginUser = ({email, password}) => {
     return (dispatch) => {
         dispatch({ type: LOGIN_USER_STATUS });
 
-        // Below firebase .then.catch is not working as expected
+        // signInWithEmailAndPassword not working
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(user => {
                 console.log("Inside .then after signInWithEmailAndPassword and before loginUserSuccess()");
@@ -57,4 +57,5 @@ const loginUserSuccess = (dispatch, user) => {
         type: LOGIN_USER_SUCCESS,
         payload: user
     });
+    Actions.main();
 };
